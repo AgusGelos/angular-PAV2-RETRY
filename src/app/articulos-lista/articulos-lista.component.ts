0a3ca7469d39d3c6b46ac16bcd4f38b043739431
AgusGelos <juanagustingelos1@gmail.com>
2020-05-06 17:22:30 -0300

    6 files modified, 3 added

--- src/app/articulos-lista/articulos-lista.component.ts
+++ src/app/articulos-lista/articulos-lista.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {ArticulosFamilia} from '../modelos/articulofamilia'
 import {ArticulosFamilias} from '../modelos/articulosfamilia-colection'
 
+import {ActivatedRoute} from '@angular/router'
+
 @Component({
   selector: 'app-articulos-lista',
   templateUrl: './articulos-lista.component.html',
@@ -11,11 +13,15 @@ export class ArticulosListaComponent implements OnInit {
   listaArticulos: ArticulosFamilia[];
   verGrilla = false;
   mensajebtn = 'Mostrar Grilla'
+  id:string;
 
   constructor() { }
 
   ngOnInit() {
-    this.listaArticulos = ArticulosFamilias
+    this.listaArticulos = ArticulosFamilias;
+    this.id = this.route.snapshot.paramMap.get("idArticulo")
+ 
+
   }
 
   MostrarGrilla(){
